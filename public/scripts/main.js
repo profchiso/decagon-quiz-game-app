18e6e778c48e9d61a2c455bfa880d481cefd469c
Okorie Chinedu sunday <okoriechinedusunday@gmail.com>
2019-06-17 09:11:36 +0100

    Add script to add question to the database

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,8 +2,15 @@ $(document).ready(function(){
     var today= new Date;
     $("#date-display").text(today.getFullYear());
 
+    //scrpit to pull out questions from the database to the actions page
 
-//function to add user to database
+
+    // End of scrpit to pull out questions from the database to the actions page
+
+
+
+
+    //function to add user to database
     $('#reg-form-btn').click(function(){
         var email=$('#email').val();
         var userName=$('#player-name').val();
@@ -30,6 +37,37 @@ $(document).ready(function(){
         }
     });
     //end of function to register user
+
+    //function to add question to database
+    $('#add-question-btn').click(function(){
+        var question=$('#question').val();
+        var optionA=$('#A').val().toUpperCase();
+        var optionB=$('#B').val().toUpperCase();
+        var optionC=$('#C').val().toUpperCase();
+        var optionD=$('#D').val().toUpperCase();
+        var answer=$('#answer').val().toUpperCase();
+
+        if(question="" && optionA!="" && optionB!="" && optionC!="" && optionD!="" && answer!="" ){
+            $.ajax({
+                url: 'http://localhost:3000/questions',
+                dataType: 'json',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify( { "question": question, "optionA": optionA, "optionB":optionB , "optionC":optionC , "optionD":optionD, "answer":answer } ),
+                success: function(){
+                    var sms=" Question is successfully added";
+                   alert(sms );
+                   window.location.assign("http://localhost:3000/pages/actions.html");
+                },
+                error: function(){
+                    alert('Error Submitting your question');
+                }
+            });
+        }else{
+            alert("All filleds are Required")
+        }
+    });
+    //end of function to add question
 
 
 
