7668336287a9e7d38d091c6dad0910892143d4a4
Okorie Chinedu sunday <okoriechinedusunday@gmail.com>
2019-06-17 09:46:42 +0100

    Create function pullQuestion

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,43 @@
 $(document).ready(function(){
     var today= new Date;
+    var xhr = new XMLHttpRequest();
     $("#date-display").text(today.getFullYear());
+    pullQuestions();
 
     //scrpit to pull out questions from the database to the actions page
-
-
+   function pullQuestions(){
+    xhr.open('GET', 'http://localhost:3000/questions', true);
+    xhr.onload = function() {
+        if(this.status == 200){
+            var questions = JSON.parse(this.responseText);
+    
+            var output = '';
+            for (var i in questions){
+                output += 
+                `<tr>
+                <td>`+questions[i].id+`</td>
+                <td>`+questions[i].question+`</td>
+                <td>`+questions[i].optionA+`</td>
+                <td>`+questions[i].optionB+`</td>
+                <td>`+questions[i].optionC+`</td>
+                <td>`+questions[i].optionD+`</td>
+                <td>`+questions[i].answer+`</td>
+                <form>
+                <td><button type="button" class="btn btn-primary btn-xs" id="update-question-btn" name="update-question-btn">Update</button>
+                <button type="button" class="btn btn-danger btn-xs" id="delete-question-btn" name="delete-question-btn">Delete</button>
+                </td>
+                </form>
+                
+            </tr>
+            `
+            }
+            document.getElementById('populate').innerHTML = output;
+        }
+    }
+    xhr.send();
+} 
     // End of scrpit to pull out questions from the database to the actions page
-
-
-
-
+    
     //function to add user to database
     $('#reg-form-btn').click(function(){
         var email=$('#email').val();
